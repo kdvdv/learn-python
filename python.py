d78f1f8a8be209c54cdac413a0b8d6dd6ec2825c
kdv <kudryadv@mail.ru>
2023-07-10 17:57:47 +1000

    Dictionary Items

--- python.py
+++ python.py
@@ -13,3 +13,27 @@ thisdictt = dict(name="John", age=36, country="Norway")
 print(thisdict["colours"])
 print(thisdictt)
 print(thisdictt["age"])
+
+x = thisdict["brand"]
+y = thisdict.get("year")
+
+print(x, y, "DDDDDDDDDD")
+
+
+k = thisdict.keys()
+print(k)
+thisdict["color"] = "pink"
+print(thisdict)
+
+x = thisdict.values()
+print(x)
+print("ddddd", thisdict)
+# Смена значения в значении ключа
+thisdict["year"] = 1985
+print(thisdict)
+# Возвращение каждого элемента в виде кортежа
+print(thisdict.items())
+
+# Проверка наличия ключа в словаре
+if "year" in thisdict:
+    print("Yes")
