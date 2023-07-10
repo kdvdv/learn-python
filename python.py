d45c4f49c4a0f5b212b53ae5fa566570d1cd3d38
kdv <kudryadv@mail.ru>
2023-07-10 13:14:51 +1000

    2.2 sstring

--- python.py
+++ python.py
@@ -1,16 +1,7 @@
-a = 2
-b = 2 + 3j
-print("a + b")
-c = 15.3
-print(type(a))
-print(type(b))
-print(type(c))
-
-
-d = input("input number: ")
-f = input("input number: ")
-print(d + f)
-
-g = int(input("input number: "))
-h = int(input("input number: "))
-print(g + h)
+s1 = 'Alice said: "Hi, Anne!"'
+s2 = "Anne answered: 'Hi, Alice'"
+print(s1)
+print(s2)
+s3 = s2 + s1
+print(s3)
+s3
