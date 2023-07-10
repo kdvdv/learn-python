6bac090a84f0d131dfff022781a64be172c3a187
kdv <kudryadv@mail.ru>
2023-07-10 12:13:05 +1000

    Glava 2.0

---
+++ python.py
@@ -0,0 +1,16 @@
+a = 2
+b = 2 + 3j
+print("a + b")
+c = 15.3
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+d = input("input number: ")
+f = input("input number: ")
+print(d + f)
+
+g = int(input("input number: "))
+h = int(input("input number: "))
+print(g + h)
