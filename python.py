b43e23faa01b221913dc3bb92c6502dae3a8723d
kdv <kudryadv@mail.ru>
2023-07-11 21:16:24 +1000

    If , Else Statement

--- python.py
+++ python.py
@@ -1,39 +1,69 @@
-thisdict = {
-    "brand": "Ford",
-    "model": "Mustang",
-    "year": 1964,
-    "colours": ["red", "white", "blue"],
-}
-print(thisdict["year"])
-print(len(thisdict))
-print(type(thisdict))
-
-thisdictt = dict(name="John", age=36, country="Norway")
-
-print(thisdict["colours"])
-print(thisdictt)
-print(thisdictt["age"])
-
-x = thisdict["brand"]
-y = thisdict.get("year")
-
-print(x, y, "DDDDDDDDDD")
+a = 33
+b = 33
+if b > a:
+    print("\n b is greater then a")
 
 
-k = thisdict.keys()
-print(k)
-thisdict["color"] = "pink"
-print(thisdict)
+print()
 
-x = thisdict.values()
-print(x)
-print("ddddd", thisdict)
-# Смена значения в значении ключа
-thisdict["year"] = 1985
-print(thisdict)
-# Возвращение каждого элемента в виде кортежа
-print(thisdict.items())
+if b > a:
+    print(" \n b is greater then a")
+elif a == b:
+    print(" a and b equal")
 
-# Проверка наличия ключа в словаре
-if "year" in thisdict:
-    print("Yes")
+# else
+a = 200
+b = 33
+if b > a:
+    print(" \n b is greater then a")
+elif a == b:
+    print(" a and b equal")
+else:
+    print("\n a is greater than b")
+
+print("A") if a > b else print("B")
+
+# else in if multiple statements
+print()
+
+a = 3301
+b = 330
+print("A > B") if a > b else print(str(a), " = ", str(b)) if a == b else print("B")
+
+print()
+# And, Or, Not,
+
+a = 200
+b = 33
+c = 600
+if a > b and c > a:
+    print("Both conditions are True")
+
+print()
+a = 200
+b = 33
+c = 600
+if a > b or c > a:
+    print("At least one of the conditions is True")
+
+print()
+a = 200
+b = 33
+c = 600
+if not a > b:
+    print("A is NOT greater than B")
+else:
+    print("b is NOT greater than a")
+
+# Nested IF
+print()
+
+x = 5
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print(" but not above 20.")
+else:
+    print("Above 10!")
