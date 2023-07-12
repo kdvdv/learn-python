51150da5502330ad7a208581404ad5bb4c497ce5
kdv <kudryadv@mail.ru>
2023-07-12 11:26:57 +1000

    While Loop

--- python.py
+++ python.py
@@ -1,69 +1,34 @@
-a = 33
-b = 33
-if b > a:
-    print("\n b is greater then a")
+# While Loop
+print("While")
 
+i = 0
+while i < 10:
+    print(i, end=", ")
+    i += 1
+# While with brake
+print("\nBrake")
 
-print()
+i = 1
+while i < 10:
+    print(i, end=(", "))
+    if i == 3:
+        break
+    i += 1
 
-if b > a:
-    print(" \n b is greater then a")
-elif a == b:
-    print(" a and b equal")
+# While with continue
+print("\nContinue")
 
-# else
-a = 200
-b = 33
-if b > a:
-    print(" \n b is greater then a")
-elif a == b:
-    print(" a and b equal")
+i = 0
+while i < 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i, end=(", "))
+# Else statement
+print("\n Else")
+i = 1
+while i < 10:
+    print(i, end=(", "))
+    i += 1
 else:
-    print("\n a is greater than b")
-
-print("A") if a > b else print("B")
-
-# else in if multiple statements
-print()
-
-a = 3301
-b = 330
-print("A > B") if a > b else print(str(a), " = ", str(b)) if a == b else print("B")
-
-print()
-# And, Or, Not,
-
-a = 200
-b = 33
-c = 600
-if a > b and c > a:
-    print("Both conditions are True")
-
-print()
-a = 200
-b = 33
-c = 600
-if a > b or c > a:
-    print("At least one of the conditions is True")
-
-print()
-a = 200
-b = 33
-c = 600
-if not a > b:
-    print("A is NOT greater than B")
-else:
-    print("b is NOT greater than a")
-
-# Nested IF
-print()
-
-x = 5
-if x > 10:
-    print("Above ten,")
-    if x > 20:
-        print("and also above 20!")
-    else:
-        print(" but not above 20.")
-else:
-    print("Above 10!")
+    print("\ni is no longer than 10")
