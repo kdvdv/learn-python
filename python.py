0f1be2eb3c29a38ebdb4eed970e2d95e11bc75c8
kdv <kudryadv@mail.ru>
2023-07-13 11:48:28 +1000

    For Loops

--- python.py
+++ python.py
@@ -1,13 +1,56 @@
-# string format
-print("\n----- Format")
-age = 34
-txt = "My name in John, and I am {}"
-print("\n" + txt.format(age))
+# For loops
+print("\n-----For loop\n")
 
-quantity = 3
-item = 234
-price = 45.56
-myorder = "I want {} pieces of item {} for {} dollars."
-myorder = "I want {1} pieces of item {0} for {2} dollars."
-print(myorder.format(quantity, item, price))
-print(myorder.format(price, item, quantity))
+b = "banana"
+fruits = ["apple", b, "cherry"]
+for x in fruits:
+    print(x, end=", ")
+
+# looping through
+print("\n\n----- Looping through\n")
+for x in b:
+    print(x, end=", ")
+
+# Break
+print("\n\n-----Break\n")
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+
+
+# Range Function
+print("\n-----Range\n")
+
+for x in range(6):
+    print(x, end=",")
+
+print("\nrange 2-6")
+for x in range(2, 6):
+    print(x, end=",")
+print("\n\nrange 2-4, step 4")
+for x in range(2, 30, 4):
+    print(x, end=",")
+
+print("\n\n----- else in for")
+for x in range(6):
+    print(x, end=" ")
+else:
+    print("\nFinally")
+
+# Nested Loops
+print("\n-----Nested Loops\n")
+
+adj = ["red", "big", "tasty"]
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+# pass
+for x in [0, 1, 2]:
+    pass
